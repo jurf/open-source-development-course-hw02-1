409f444b78a1d428c919ae0455d013415df712f9
Juraj Fiala <jurf@riseup.net>
2020-03-23 21:20:16 +0100

    Implement HW1

--- ossdev/__main__.py
+++ ossdev/__main__.py
@@ -6,8 +6,8 @@ def main():
     b = Vector([4, 3, 2, 1, 0])
     print('Vector a: %s' % a)
     print('Vector b: %s' % b)
-    print('Vector a+2: %s' % (a+2))
-    print('Vector a+b: %s' % (a+b))
+    print('Vector a+2: %s' % (a + 2))
+    print('Vector a+b: %s' % (a + b))
 
     m = Matrix((3, 3))
     print(m)
---
+++ ossdev/tests/test_vct.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: ossdev
+
+import unittest
+from ossdev import Vector
+
+import math
+
+
+class VectorTest(unittest.TestCase):
+    """Simple vector tests"""
+
+    def __init__(self, *args, **kwargs):
+        super(VectorTest, self).__init__(*args, **kwargs)
+
+    def test_cmp(self):
+        a = Vector([0, 1, 2, 3])
+        b = Vector([3, 2, 1, 0])
+        self.assertEquals(a.__cmp__(b), -1)
+
+    def test_reversed(self):
+        a = reversed(Vector([1, 2, 3, 4, 5, 6, 7]))
+        self.assertEqual(a.get(), [7, 6, 5, 4, 3, 2, 1])
+
+    def test_add(self):
+        a = Vector([0, 1, 2, 3])
+        b = Vector([3, 2, 1, 0])
+        c = a + b
+
+        self.assertEqual(c.get(), [3, 3, 3, 3])
+
+    def test_setitem(self):
+        a = Vector([1, 1, 1, 1, 1])
+        a[2] = 0
+        self.assertEqual(a.get(), [1, 1, 0, 1, 1])
+
+    def test_sub(self):
+        a = Vector([0, 1, 2, 3])
+        b = Vector([-3, -2, -1, -0])
+        c = a - b
+
+        self.assertEqual(c.get(), [3, 3, 3, 3])
+
+    def test_mul(self):
+        a = Vector([1, 2, 3, 4])
+        b = 7
+
+        self.assertEqual((a * b).get(), [7, 14, 21, 28])
+
+    def test_xor(self):
+        a = Vector([1, 2, 3])
+        b = 2
+
+        self.assertEqual((a ^ b).get(), [3, 0, 1])
+
+    def test_length(self):
+        a = Vector([3, 4])
+        self.assertEqual(a.length(), 5)
+
+        a = Vector([1, 2, 3, 4, 5])
+        self.assertAlmostEqual(a.length(), math.sqrt(55))
+
+    def test_dot(self):
+        a = Vector([1, 2])
+        b = Vector([2, -1])
+        self.assertEqual(a.dot(b), 0)
+
+    def test_lenght(self):
+        # Uncomment after passing
+        # self.assertAlmostEqual(Vector([2, 4]).length(), 4.47213595499, 3)
+        # self.assertAlmostEqual(Vector([2, 4, 5]).length(), 6.708203932499369, 3)
+        return
+
+
+
+if __name__ == "__main__":
+    unittest.main()  # pragma: no cover
